c9fedc8e419558a780bf0e48d81e8e8b5ecd7476
David Yu <davidyu37@gmail.com>
2019-07-16 17:39:01 +0800

    trim the fat

--- routes/wechat.js
+++ routes/wechat.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 // WeChat Stuff
 const { Wechat, MiniProgram } = require('wechat-jssdk');
-const WXDecrypt = require('../utils/decrypt');
 
 const wechatConfig = {
   "appId": process.env.MP_APPID,
@@ -14,11 +13,6 @@ const wechatConfig = {
   }
 };
 const wx = new Wechat(wechatConfig);  
-
-// router.get('/', async(req, res, next) => {
-//   const { access_token } = await wx.jssdk.getAccessToken();
-//   res.status(200).json(access_token);
-// });
 
 router.post('/login', async (req, res) => {
   const { code } = req.query;
@@ -37,67 +31,6 @@ router.post('/login', async (req, res) => {
     }
   }
   res.json('No Code Found');
-});
-
-// Template Message
-router.post('/save-formId', async (req, res) => {
-  // save form ID here, to send a message to a user who didn't trigger the form.
-})
-
-router.use('/send-message', async (req, res) => {
-  /*
-  {
-    "form_id": "1d16026cadf5fef48705f8dc416120dc",
-    "touser": 'oIEcc5CTl4nRK1hORoSghj19N-GA', // openID
-    "template_id":'epDAg_fFYdvsVD5qDLS2jpxXU45wfNWME36q1HMjgTg',
-    "data": {
-      keyword1: {
-        value: 'Product Name',
-      },
-      keyword2: {
-        value: 'Product Description',
-      },
-      keyword3: {
-        value: '10/10/2018',
-      },
-      keyword4: {
-        value: '10/10/2018',
-      },
-    },
-  }
-  */
-  try {
-    // Get access token
-   const { access_token } = await wx.jssdk.getAccessToken();
-  //  console.log(access_token);
-   // Get template id either hardcoded or making a request to https://api.weixin.qq.com/cgi-bin/wxopen/template/list?access_token=ACCESS_TOKEN
-   const template_id = "rOCU8DIXCI1FBIxhg8zpUGyqnYhqT2obhj70Hn8VK2M";
-   // Send message
-   const message = {
-      form_id: req.body.form_id,
-      // Recipient's OpenID
-      touser: req.body.touser,
-      template_id,
-      data: {
-        keyword1: {
-          value: '10/10/2018',
-        },
-        keyword2: {
-          value: 'Product Name',
-        },
-        keyword3: {
-          value: 'Delivery Platform',
-        },
-      }
-    };
-
-   const response = await axios.post(`https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?access_token=${access_token}`, message);
-
-    res.status(200).json(response.data);
- } catch (e) {
-   console.error(e.message || e);
-   res.status(500).json({ error: e.message || e });
- }
 });
 
 module.exports = router;
