6b91a79adaaf66596df9b0d6d1ebbb39b50518ed
David Yu <davidyu37@gmail.com>
2019-07-18 16:59:15 +0800

    order route change

--- routes/order.js
+++ routes/order.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db.json');
 
-router.get('/all', async(req, res, next) => {
+router.get('/', async(req, res, next) => {
   const orders = db['orders'];
 
   res.status(200).json(orders);
